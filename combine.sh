4e74943db2d8758f9019203332f406ffa6afb39d
Franco Mossotto <fmossott@users.noreply.github.com>
2020-12-15 19:16:43 +0100

    Path for 20201205 tested cases

--- combine.sh
+++ combine.sh
@@ -9,9 +9,16 @@ if [ ! -d $TGT_DIR ]; then
   mkdir $TGT_DIR
 fi
 
+# Apply patches
+for f in patches/*.patch; do
+  git -C COVID-19 apply ../$f
+done
+
 banner "Elaborating Regions data"
 python3 pyscript/regiondata.py
 
 banner "Elaborating Provinces data"
 python3 pyscript/provincedata.py
 
+# Remove patches
+git -C COVID-19 reset --hard
