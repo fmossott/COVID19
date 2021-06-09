06b7ccea82e98f2ee6cceda538178e08bdf406f5
Franco Mossotto <fmossott@users.noreply.github.com>
2021-06-09 10:36:30 +0200

    corr

--- pyscript/corr.py
+++ pyscript/corr.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 #%%
 home=os.path.dirname(__file__)+"/../"
@@ -15,25 +17,38 @@ reg = reg.groupby('Date', as_index=False).agg('sum')
 def corr(x, y, lag=0):
   return x.corr(y.shift(lag))
 
-def plot(df, lag=0, title=""):
-  cd = df['c'].to_frame()
-  lagcol='d'+str(lag)
-  cd[lagcol] = df['d'].shift(lag)
-  cd.plot.scatter(x='c',y=lagcol,title=title)
+fig=0
 
+def plot(df, lag=0, title=""):
+  cd=df['c'].to_frame()
+  cd['d']=df['d'].shift(lag)
+  cd=cd.dropna()
+
+  x = cd['c'].values.reshape(-1, 1)  # values converts it into a numpy array
+  y = cd['d'].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
+  linear_regressor = LinearRegression(fit_intercept=False)  # create object for the class
+  linear_regressor.fit(x, y)  # perform linear regression
+  y_pred = linear_regressor.predict(x)  # make predictions
+
+  lrcoeff = linear_regressor.predict([[1]])[0][0]
+
+  global fig
+  fig+=1
+  plt.figure(fig)
+  plt.title(title+" linreg coeff: {:.1f}%".format(lrcoeff*100))
+  plt.scatter(x, y)
+  plt.plot(x, y_pred, color='red')
 #%%
 def findlag(r, title):
   idx=range(31)
   df = pd.DataFrame({'lag': ['d-'+str(i) for i in idx], 'corr': [corr(r['c'], r['d'], -i) for i in idx]}, index=idx)
 
   lag = df['corr'].idxmax()
-  t="Corelation coeff for %s: %.5f with lag %d days" % ( title, df['corr'].max(), lag )
+  t="Correlation coeff for %s: %.5f with lag %d days" % ( title, df['corr'].max(), lag )
 
-  fatality = (r['d'].shift(lag)/r['c']).mean()
-  fatality2 = (r['d'].sum()/r['c'].sum())
-  t+="\navg fatality rate {:.1f}% / fatality rate: {:.1f}%".format(fatality*100,fatality2*100)
+  fatality = (r['d'].sum()/r['c'].sum())
+  t+="\navg fatality rate: {:.1f}%".format(fatality*100)
   
-  print(t)
   plot(r, -lag, t)
 
 # %%
