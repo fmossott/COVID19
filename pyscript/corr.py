398d37699358e8749432ee069d9c09687a98becf
Franco Mossotto <fmossott@users.noreply.github.com>
2021-06-09 09:58:40 +0200

    corr

--- pyscript/corr.py
+++ pyscript/corr.py
@@ -1,30 +1,54 @@
 #%%
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 #%%
-reg = pd.read_csv('../combined/regions_ts')
+home=os.path.dirname(__file__)+"/../"
+
+#%%
+reg = pd.read_csv(home+'/combined/regions_ts')
 reg = reg[['Date','Region','Weekly Cases','Weekly Deaths']].rename(columns={'Weekly Cases': 'c','Weekly Deaths': 'd'})
-reg = reg[reg['Date']>'2020-08-01']
-reg = reg.groupby('Date').agg('sum')
+reg = reg.groupby('Date', as_index=False).agg('sum')
 
 #%%
 def corr(x, y, lag=0):
   return x.corr(y.shift(lag))
 
-def plot(df, lag=0):
+def plot(df, lag=0, title=""):
   cd = df['c'].to_frame()
   lagcol='d'+str(lag)
   cd[lagcol] = df['d'].shift(lag)
-  cd.plot.scatter(x='c',y=lagcol,title=lagcol)
+  cd.plot.scatter(x='c',y=lagcol,title=title)
 
 #%%
-idx=range(31)
-df = pd.DataFrame({'lag': ['d-'+str(i) for i in idx], 'corr': [corr(reg['c'], reg['d'], -i) for i in idx]}, index=idx)
+def findlag(r, title):
+  idx=range(31)
+  df = pd.DataFrame({'lag': ['d-'+str(i) for i in idx], 'corr': [corr(r['c'], r['d'], -i) for i in idx]}, index=idx)
+
+  lag = df['corr'].idxmax()
+  t="Corelation coeff for %s: %.5f with lag %d days" % ( title, df['corr'].max(), lag )
+
+  fatality = (r['d'].shift(lag)/r['c']).mean()
+  fatality2 = (r['d'].sum()/r['c'].sum())
+  t+="\navg fatality rate {:.1f}% / fatality rate: {:.1f}%".format(fatality*100,fatality2*100)
+  
+  print(t)
+  plot(r, -lag, t)
 
 # %%
-lag = df['corr'].idxmax()
-print("max corr "+str(df['corr'].max())+" for lag -"+str(lag))
+findlag(reg,'all')
+
 # %%
-for l in range(31):
-  plot(reg, -l)
+findlag(reg[reg['Date']<='2020-07-31'],'1st (until 2020-07-31)')
 # %%
+findlag(reg[(reg['Date']>='2020-08-01')],'2nd and 3rd (since 2020-08-01)')
+
+# %%
+findlag(reg[(reg['Date']>='2020-08-01') & (reg['Date']<='2021-02-28')],'2nd (2020-08-01 to 2021-02-28')
+
+# %%
+findlag(reg[reg['Date']>='2021-03-01'],'3nd (since 2021-03-01')
+
+#%%
+plt.show()
